1d3759f521751f93dfd581afc689a0d939e456ce
Bruno Silva <brunomiguelsilva@ua.pt>
2016-02-16 11:11:16 +0000

    fixed prints in snmp

--- Trabalho1/base_files/baseSNMP.py
+++ Trabalho1/base_files/baseSNMP.py
@@ -29,82 +29,56 @@ def main():
 
     sys.stdout = Logger("router_" + args.router)
 
-    print(args)
-
     #Creates SNMP manager for router with address args.router
     m=M(args.router,'private',3, secname='uDDR',authprotocol="MD5", authpassword="authpass",privprotocol="AES", privpassword="privpass")
 
     print(m.sysDescr)   #Gets sysDescr from SNMPv2-MIB
 
-    print("===")
-
-    #print(m.ifDescr.items()) #Lists (order, name) interfaces in ifDescr from IF-MIB
-
-    for i, name in m.ifDescr.items():
-        print("Interface order %d: %s" % (i, name))
-
-    print("===")
-
-    #print(m.ipAddressIfIndex.items()) #Lists ((adr.type,adr),order) interfaces in ipAddressIfIndex from IP-MIB
-
     ifWithAddr={}   #Stores (order, first adr.) of all interfaces
     for addr, i in m.ipAddressIfIndex.items():
         if not i in ifWithAddr:
             ifWithAddr.update({i:IPfromOctetString(addr[0],addr[1])})
-        print('%s, Interface order: %d, %s'%(IPfromOctetString(addr[0],addr[1]),i,m.ifDescr[i]))
-
-    #print(ifWithAddr)
 
     t = 0
     try:
 
         while True:
-            print("=== %d Seconds passed ===" % t)
-
-            ifOutUCastPkts={}  #Stores (order, first adr.) of all interfaces
+            ifOutUCastPkts={} #Stores (order, OutPkts) of all interfaces
             for i, pkts in m.ifHCOutUcastPkts.items():
                 if i in ifWithAddr.keys():
                     if not i in ifOutUCastPkts:
                         ifOutUCastPkts.update({i:pkts})
-                    print('%s, Interface Out packets: %d'%(m.ifDescr[i],pkts))
 
-            print("===")
-
-            ifInUCastPkts={}  #Stores (order, first adr.) of all interfaces
+            ifInUCastPkts={} #Stores (order, InPkts) of all interfaces
             for i, pkts in m.ifHCInUcastPkts.items():
                 if i in ifWithAddr.keys():
                     if not i in ifInUCastPkts:
                         ifInUCastPkts.update({i:pkts})
-                    print('%s, Interface In packets: %d'%(m.ifDescr[i],pkts))
 
-            print("===")
-
-            ifOutOctets={}  #Stores (order, first adr.) of all interfaces
+            ifOutOctets={} #Stores (order, OutOctets) of all interfaces
             for i, pkts in m.ifHCOutOctets.items():
                 if i in ifWithAddr.keys():
                     if not i in ifOutOctets:
                         ifOutOctets.update({i:pkts})
-                    print('%s, Interface Out octets: %d'%(m.ifDescr[i],pkts))
 
-            print("===")
-
-            ifInOctets={}  #Stores (order, first adr.) of all interfaces
+            ifInOctets={} #Stores (order, InOctets) of all interfaces
             for i, pkts in m.ifHCInOctets.items():
                 if i in ifWithAddr.keys():
                     if not i in ifInOctets:
                         ifInOctets.update({i:pkts})
-                    print('%s, Interface In octets: %d'%(m.ifDescr[i],pkts))
 
-            print("===")
-
-            ifQstats={}  #Stores (order, first adr.) of all interfaces
+            ifQstats={} #Stores (order, queue_size) of all interfaces
             for (i,u),pkts in m.cQStatsDepth.items():
                 if i in ifWithAddr.keys():
                     if not i in ifQstats:
                         ifQstats.update({i:pkts})
-                    print('%s, Interface Queue Size: %d'%(m.ifDescr[i],pkts))
 
-            print("===")
+            print("=== %d Seconds passed ===" % t)
+            for i, name in m.ifDescr.items():
+                if i in ifWithAddr:
+                    print("%s, %s - pkts[in/out][%s/%s] - octets[in/out][%s/%s] - queue[%s]" % (ifWithAddr[i], name, ifInUCastPkts[i], ifOutUCastPkts[i], ifInOctets[i], ifOutOctets[i], ifQstats[i]))
+
+            print("========================")
 
             time.sleep(args.sinterval)
             t += args.sinterval
