058353aaeaec5127e110c140cb473b236678d8a0
Bernardomrf <bernardomrferreira@ua.pt>
2016-03-04 09:45:53 +0000

    pcap done

--- Trabalho1/base_files/basePCap.py
+++ Trabalho1/base_files/basePCap.py
@@ -1,23 +1,82 @@
 import sys
 import argparse
 import pyshark
+import numpy as np
 from netaddr import IPNetwork, IPAddress, IPSet
 
 npkts=0
+downloadTick=0
+uploadTick=0
+downloadTotal=0
+uploadTotal=0
+tick=10
+targetDownload = open("Download", 'w+')
+targetUpload = open("Upload", 'w+')
+first=True
+downloadValues= []
 
 def pkt_callback(pkt):
 	global scnets
 	global ssnets
 	global npkts
+	global downloadTick
+	global uploadTick
+	global downloadTotal
+	global uploadTotal
+	global tick
+	global targetDownload
+	global targetUpload
+	global first
+	global downloadValues
 
+	
 	if IPAddress(pkt.ip.src) in scnets|ssnets and IPAddress(pkt.ip.dst) in scnets|ssnets:
 		npkts=npkts+1
-		if pkt.ip.proto=='17':
-			print('%s: IP packet from %s to %s (UDP:%s) %s'%(pkt.sniff_timestamp,pkt.ip.src,pkt.ip.dst,pkt.udp.dstport,pkt.ip.len))
-		elif pkt.ip.proto=='6':
-			print('%s: IP packet from %s to %s (TCP:%s) %s'%(pkt.sniff_timestamp,pkt.ip.src,pkt.ip.dst,pkt.tcp.dstport,pkt.ip.len))
+		if first:
+			tick = float(pkt.sniff_timestamp)
+			first = False
 		else:
-			print('%s: IP packet from %s to %s (other) %s'%(pkt.sniff_timestamp, pkt.ip.src,pkt.ip.dst,pkt.ip.len))
+			diference = float(pkt.sniff_timestamp) - float(tick)
+			
+			if diference > 0.1:
+				if diference > 0.2:
+					i=0.1
+					while i < diference:
+						downloadValues.append(0)
+						targetDownload.write("0")
+						targetUpload.write("0")
+						targetDownload.write(" ")
+						targetUpload.write(" ")
+						i=i+0.1
+					
+				downloadValues.append(float(downloadTick))
+				targetDownload.write(str(downloadTick))
+				targetDownload.write(" ")
+				targetUpload.write(str(uploadTick))
+				targetUpload.write(" ")
+   
+				downloadTick = 0
+				uploadTick = 0
+				tick = pkt.sniff_timestamp
+			
+			if IPAddress(pkt.ip.src) in ssnets:
+
+				downloadTick=downloadTick+int(pkt.ip.len)
+				downloadTotal=downloadTotal+int(pkt.ip.len)
+			
+			if IPAddress(pkt.ip.dst) in ssnets:
+
+				uploadTick=uploadTick+int(pkt.ip.len)
+				uploadTotal=uploadTotal+int(pkt.ip.len)			
+				
+			if pkt.ip.proto=='17':
+				print('%s: IP packet from %s to %s (UDP:%s) %s'%(pkt.sniff_timestamp,pkt.ip.src,pkt.ip.dst,pkt.udp.dstport,pkt.ip.len))
+			elif pkt.ip.proto=='6':
+				print('%s: IP packet from %s to %s (TCP:%s) %s'%(pkt.sniff_timestamp,pkt.ip.src,pkt.ip.dst,pkt.tcp.dstport,pkt.ip.len))
+			else:
+				print('%s: IP packet from %s to %s (other) %s'%(pkt.sniff_timestamp, pkt.ip.src,pkt.ip.dst,pkt.ip.len))
+				
+
 
 def main():
 	parser=argparse.ArgumentParser()
@@ -66,12 +125,20 @@ def main():
 	
 	cint=args.interface
 	print('Filter: %s on %s'%(cfilter,cint))
+	
 	try:
 		capture = pyshark.LiveCapture(interface=cint,bpf_filter=cfilter)
 		capture.apply_on_packets(pkt_callback)
 	except KeyboardInterrupt:
 		global npkts
-		print('\n%d packets captured! Done!\n'%npkts)
+		global downloadTotal
+		global uploadTotal
+		global targetDownload
+		global targetUpload
+		global downloadValues
+		targetDownload.close()
+		targetUpload.close()
+		print('\n%d packets captured! Done!\n Download: %d Bytes\n Upload: %d Bytes'%(npkts, downloadTotal, uploadTotal))
 
 if __name__ == '__main__':
     main()
